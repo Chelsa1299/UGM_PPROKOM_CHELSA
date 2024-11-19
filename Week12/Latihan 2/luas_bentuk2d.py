250c6d7adf9c65fbe323aa40380f792b4299067f
Chelsa1299 <chelsayoga81@gmail.com>
2024-11-19 15:07:15 +0700

    Add files via upload

---
+++ Week12/Latihan 2/luas_bentuk2d.py
@@ -0,0 +1,22 @@
+def menu():
+    print("Pilih Bentuk 2D")
+    print("1. Persegi Panjang")
+    print("2. Lingkaran")
+    print("3. Segitiga")
+    print("4. Keluar")
+    pilihan = input("Masukkan pilihan (1/2/3/4): ")
+    return pilihan
+def persegi():
+    print("Menghitung Luas Persegi Panjang")
+    p = float(input("Masukkan Panjang:) 1 = float(input("Masukkan Lebar: "))
+    luas = p * 1
+    print("Luas Persegi Panjang adalah:", luas)
+def lingkaran(): print("Menghitung Luas Lingkaran")
+    r = float(input("Masukkan Jari-Jari: "))
+    luas = 3.14* (** 2) print("Luas Lingkaran adalah:", luas)
+def segitiga():
+    print("Menghitung Luas Segitiga")
+    a = float(input("Masukkan Alas: "))
+    t = float(input("Masukkan Tinggi: "))
+    luas (at) / 2
+    print("Luas Segitiga adalah:", luas)
